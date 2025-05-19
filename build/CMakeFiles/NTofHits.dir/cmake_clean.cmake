033cb6c34c96b374adfd935386dd287e10132ccf
Grace <gtytus@hawaii.edu>
2025-05-19 10:10:30 -1000

    setting up tof-xmas directory

---
+++ build/CMakeFiles/NTofHits.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NTofHits.dir/nhits.cc.o"
+  "CMakeFiles/NTofHits.dir/nhits.cc.o.d"
+  "NTofHits"
+  "NTofHits.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NTofHits.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
